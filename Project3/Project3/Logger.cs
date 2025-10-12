7f313be1d6aa522eb6e279fa60c4566a8d222542
kaacaa <kacanovakoviic@gmail.com>
2025-10-12 13:56:49 +0200

    n

--- Project3/Project3/Logger.cs
+++ Project3/Project3/Logger.cs
@@ -2,8 +2,13 @@
 
 public static class Logger
 {
+    private static readonly object _lock = new object();
+
     public static void Log(string message)
     {
-        Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {message}");
+        lock (_lock)
+        {
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {message}");
+        }
     }
 }
